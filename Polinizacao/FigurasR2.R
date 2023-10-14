79b42bf43c618a88a8bd7eb722d96badf66f2ba9
Iracy Maiany <iracymaiany@hotmail.com>
2023-10-14 11:51:26 -0300

    Ajuste de temas

--- Polinizacao/FigurasR2.R
+++ Polinizacao/FigurasR2.R
@@ -14,22 +14,22 @@ hist_fill_color <- "#C88325"  # Laranja
 
 # Cria histograma para Sorensen
 sorensen_hist <- ggplot(metricas2, aes(x = SORENSEN_mean)) +
-  geom_histogram(binwidth = 0.1, fill = hist_fill_color, color = "gray") +
+  geom_histogram(bins = 10, binwidth = 0.1, fill = hist_fill_color) +
   labs(x = "Sorensen", y = "Frequencia") +
   theme_gray()
 
 # Converta a variável "TSS_mean" em numérica
 metricas2$TSS_mean <- as.numeric(metricas2$TSS_mean)
 
-# Agora, você pode criar um histograma para TSS com a mesma cor
+# Histograma para TSS com a mesma cor
 tss_hist <- ggplot(metricas2, aes(x = TSS_mean)) +
-  geom_histogram(bins = 15, na.rm = TRUE, fill = hist_fill_color,  color = "gray") +
+  geom_histogram(bins = 10, na.rm = TRUE, fill = hist_fill_color) +
   labs(x = "TSS_mean", y = "Frequencia") +
   theme_gray()
 
 # Cria histograma para AUC com a mesma cor
 auc_hist <- ggplot(metricas2, aes(x = AUC_mean)) +
-  geom_histogram(binwidth = 0.1, fill = hist_fill_color, color = "gray") +
+  geom_histogram(bins = 10, binwidth = 0.1, fill = hist_fill_color) +
   labs(x = "AUC", y = "Frequencia") +
   theme_gray()
 # Salva os gráficos
